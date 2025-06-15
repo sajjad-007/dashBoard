a429fb3eb3112ee21d820f9ee92e928ce12b4c35
sajjad-007 <sajjadhossain8123@gmail.com>
2025-06-15 22:39:47 +0600

    dash boad set up

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@material-tailwind/react"
+
+
+function App() {
+
+
+  return (
+   <>
+   
+   <Button color="blue" className="py-4 px-4  text-2xl">color blue</Button>
+   </>
+  )
+}
+
+export default App
